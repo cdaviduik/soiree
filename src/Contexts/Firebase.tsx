8e1efa5319049d36a483e6aafe4d412b09267b12
Chris Daviduik <cdaviduik@gmail.com>
2023-11-08 12:20:45 -0500

    Create react app

---
+++ src/Contexts/Firebase.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, createContext, useContext } from "react";
+import { FirebaseApp, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBONo15aPSvZsuh_69HwN6_SKIZNVdXaYE",
+  authDomain: "soiree-37875.firebaseapp.com",
+  projectId: "soiree-37875",
+  storageBucket: "soiree-37875.appspot.com",
+  messagingSenderId: "141169672481",
+  appId: "1:141169672481:web:2774c3b61caf32a4ecbe60",
+};
+
+const FirebaseContext = createContext<FirebaseApp | null>(null);
+
+export const useFirebase = () => {
+  return useContext(FirebaseContext);
+};
+
+export const FirebaseProvider = ({ children }: { children: ReactNode }) => {
+  const app = initializeApp(firebaseConfig);
+
+  return (
+    <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
+  );
+};
