fb20755db72fb1391e6913964bca8a76f84b9b3e
Chris Daviduik <cdaviduik@gmail.com>
2023-12-02 17:11:35 -0500

    Remove export

--- src/Repo/Auth/index.ts
+++ src/Repo/Auth/index.ts
@@ -1,10 +1,4 @@
-export {
-  getCurrentUser,
-  getUser,
-  getUsers,
-  signInWithGoogle,
-  signOut,
-} from "./Auth";
+export { getCurrentUser, getUser, getUsers, signOut } from "./Auth";
 export { useAuth } from "./useAuth";
 export { AuthProvider } from "./AuthContext";
 export type { User } from "./AuthContext";
