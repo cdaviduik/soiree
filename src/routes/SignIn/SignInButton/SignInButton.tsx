fc8b8d7a5e3b209a3274d32d640cf7ca7290f441
Chris Daviduik <cdaviduik@gmail.com>
2023-12-03 12:40:05 -0500

    Add email auth

--- src/routes/SignIn/SignInButton/SignInButton.tsx
+++ src/routes/SignIn/SignInButton/SignInButton.tsx
@@ -14,7 +14,7 @@ const uiConfig = {
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
     // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
   ],
